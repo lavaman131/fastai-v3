7c9ecbd4963bd9109f49e9aacddb022597981835
lavaman131 <lavaalex3@gmail.com>
2020-10-13 00:10:12 -0700

    changes to UI

--- app/static/client.js
+++ app/static/client.js
@@ -1,7 +1,11 @@
 var el = x => document.getElementById(x);
 
+function refreshPage() {
+  window.location.reload();
+}
 function showPicker() {
   el("file-input").click();
+  
 }
 
 function showPicked(input) {
@@ -10,6 +14,10 @@ function showPicked(input) {
   reader.onload = function(e) {
     el("image-picked").src = e.target.result;
     el("image-picked").className = "";
+    el("analyze-button").disabled = false;
+    el("analyze-button").style.backgroundColor = "#7052cb";
+    el("analyze-button").style.cursor = "pointer";
+    
   };
   reader.readAsDataURL(input.files[0]);
 }
@@ -17,7 +25,6 @@ function showPicked(input) {
 function analyze() {
   var uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
-
   el("analyze-button").innerHTML = "Analyzing...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
@@ -30,12 +37,22 @@ function analyze() {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `Result = ${response["result"]}`;
+      el("searchButton").style.backgroundColor = "#228B22";
+      el("searchButton").style.cursor = "pointer";
+      
+      if (response["result"].includes("Healthy")) {
+        el("searchButton").style.visibility = 'hidden';
+       
+      }
+      else {
+        el("searchButton").style.visibility = 'visible';
+      }
       
       (function search(){
-        var searchButton = document.getElementById("searchButton");
+        
+        var searchButton = el("searchButton");
         searchButton.addEventListener("click", function search(){
         window.open('https://www.google.com/search?q=' + response["result"] + ' ' + 'treatment');
-        window.location.reload();
     
   });
 })();
